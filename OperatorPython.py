45bd63e3dfdb55acef01d373e822881abf0a53b0
Fahmi Subarashi <56239989+coraid12@users.noreply.github.com>
2019-10-19 18:55:39 +0700

    Create OperatorPython.py

---
+++ OperatorPython.py
@@ -0,0 +1,17 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
+a=input("Inputkan nilai a: ")
+b=input("Inputkan nilai b: ")
+c = a + b
+print "Hasil %d + %d = %d" % (a,b,c)
+c = a - b
+print "Hasil %d - %d = %d" % (a,b,c)
+c = a * b
+print "Hasil %d * %d = %d" % (a,b,c)
+c = a / b
+print "Hasil %d / %d = %d" % (a,b,c)
+c = a % b
+print "Hasil %d %% %d = %d" % (a,b,c)
+c = a ** b
+print "Hasil %d ** %d = %d" % (a,b,c)
